74df7d2edb7e3acfed2b9d211743d1371deacb53
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-09-20 11:32:54 -0400

    Build custom kernel with KSUN and SuSFS settings

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,6 +96,8 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm6375
 TARGET_KERNEL_CONFIG := vendor/holi-qgki_defconfig vendor/debugfs.config
 TARGET_KERNEL_NO_GCC := true
+# KernelSU-Next and SuSFS config
+TARGET_KERNEL_CONFIG += ksun_sus.config
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
